9464dfcae8cf3c3908fe291e04bdc408e3b49983
lindelof <phragmen@protonmail.com>
2019-12-19 17:36:20 +0800

    update readme

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
 
 import pkg from './package.json'
 
@@ -33,7 +32,6 @@ export default {
       modules: true
     }),
     url(),
-    //svgr(),
     babel({
       exclude: 'node_modules/**',
       plugins: [ 'external-helpers' ]
